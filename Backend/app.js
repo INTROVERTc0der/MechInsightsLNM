acb17b765a84562e91961c2e4cd8778376680a0c
INTROVERTc0der <21ucs030@gmail.com>
2024-10-01 14:54:20 +0530

    add backend changed file

--- Backend/app.js
+++ Backend/app.js
@@ -1,32 +1,37 @@
 import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
-//import { Cors } from "cors";
-import Student from "./models/Student.model.js"
-import studentRoutes from './routes/student.routes.js'
-import facultyRoutes from './routes/faculty.routes.js'
-import formRoutes from './routes/form.routes.js'
-import hodRoutes from './routes/hodRoutes.js'
+import cors from "cors";
+import Student from "./models/Student.model.js";
+import studentRoutes from './routes/student.routes.js';
+import facultyRoutes from './routes/faculty.routes.js';
+import formRoutes from './routes/form.routes.js';
+import hodRoutes from './routes/hodRoutes.js';
+
 config();
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors({
+    origin: 'http://localhost:5173', // allow requests from this origin
+    credentials: true, // allow credentials (cookies, authorization headers, etc.)
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-
-//routes
+// routes
 app.get('/', (req, res) => {
     res.status(404).json({ message: "hello from server" });
-})
+});
 
 app.use('/api/v1/user_student', studentRoutes);
 app.use('/api/v1/user_faculty', facultyRoutes);
-app.use('/api/v1/user_hod',hodRoutes);
+app.use('/api/v1/user_hod', hodRoutes);
 app.use('/api/v1/forms', formRoutes);
-//app.use('/api/v1/auth',authRoutes)
 
-//default catch all route -404
+// default catch all route - 404
 app.use('*', (req, res) => {
     res.status(404).send('OOPS!! 404 Page Not Found');
 });
